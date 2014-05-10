cc59147224a9a5ba5e3f20e68c35c7227686019b
Sean Rose <sean1rose@gmail.com>
2014-05-10 12:41:55 -0700

    Answer the question

---
+++ 02_variables/exercise_06.rb
@@ -0,0 +1,7 @@
+# What does the following error msg tell you?
+# NameError: undefined local variable or method `shoes' for main:Object
+# from (irb):3
+# from /usr/local/rvm/rubies/ruby-2.0.0-rc2/bin/irb:16:in `<main>'
+
+puts "It's telling me that there's an undefined variable on line 3 of the code."
+puts "Sort of like in the previous exercise, perhaps we were trying to access a variable that was created w/in a do-end loop."
