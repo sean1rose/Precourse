9603d8eedd0ea59929c5713760e05290f94245e8
Sean Rose <sean1rose@gmail.com>
2014-05-09 19:12:03 -0700

    print years using an array

---
+++ 01_the_basics/exercise_04.rb
@@ -0,0 +1,7 @@
+movies = {:The_Man_Of_Steel => 2013, :The_Dark_Knight => 2008, :The_Dark_Knight_Rises => 2012}
+
+yearArray = [movies[:The_Man_Of_Steel], movies[:The_Dark_Knight], movies[:The_Dark_Knight_Rises]]
+
+puts yearArray[0]
+puts yearArray[1]
+puts yearArray[2]
