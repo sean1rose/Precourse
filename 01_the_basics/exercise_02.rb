51aec01888f9f0cab329648eaf4cb50b6ad697b2
Sean Rose <sean1rose@gmail.com>
2014-05-09 18:50:28 -0700

    update answer

--- 01_the_basics/exercise_02.rb
+++ 01_the_basics/exercise_02.rb
@@ -1,9 +1,13 @@
 a = 1234
 
+# print out the thousands place (1)
 puts a / 1000
 
+# print out the hundreds place (2)
 puts a % 1000 / 100
 
+# print out the tenths place (3)
 puts a % 1000 % 100 / 10
 
+# print out the ones (4)
 puts a % 1000 % 100 % 10
