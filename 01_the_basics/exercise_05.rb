91f4b9b76140df099da14fc30cd82facec5d7f46
Sean Rose <sean1rose@gmail.com>
2014-05-09 19:23:23 -0700

    factorials of 5, 6, 7, 8

---
+++ 01_the_basics/exercise_05.rb
@@ -0,0 +1,9 @@
+numeros = [0,1,2,3,4,5,6,7,8]
+
+puts numeros[5] * numeros[4] * numeros[3] * numeros[2] * numeros[1]
+
+puts numeros[6] * numeros[5] * numeros[4] * numeros[3] * numeros[2] * numeros[1]
+
+puts numeros[7] * numeros[6] * numeros[5] * numeros[4] * numeros[3] * numeros[2] * numeros[1]
+
+puts numeros[8] * numeros[7] * numeros[6] * numeros[5] * numeros[4] * numeros[3] * numeros[2] * numeros[1]
