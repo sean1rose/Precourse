b44d6a89fb00fe45578fb3d6a5d55c34a2cd48db
Sean Rose <sean1rose@gmail.com>
2014-05-10 12:30:07 -0700

    First and last name printed

--- 02_variables/name.rb
+++ 02_variables/name.rb
@@ -6,3 +6,11 @@ puts "Welcome " + name + " nice of you to join us foo!"
 10.times do
 	puts name
 end
+
+puts "Give me your first name now!"
+first_name = gets.chomp
+
+puts "Now your last name..."
+last_name = gets.chomp
+
+puts "Your full name is " + first_name + ' ' + last_name
