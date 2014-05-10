6eea4bef8c292a2db42d81f127e4fbbfd4c1ea81
Sean Rose <sean1rose@gmail.com>
2014-05-10 14:02:49 -0700

    return practice

---
+++ 03_methods/exercise_02.rb
@@ -0,0 +1,5 @@
+x = 2							# returns 2  
+puts x = 2						# prints 2, returns nil
+p name = "Joe"					# prints "Joe", returns "Joe"
+four = "four"					# returns "four"		
+print something = "nothing"		# returns nil
