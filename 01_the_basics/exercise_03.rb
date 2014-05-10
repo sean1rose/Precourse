d12b7412615b657f56a386c7388845efbdfd648e
Sean Rose <sean1rose@gmail.com>
2014-05-09 19:06:58 -0700

    print out movie years

---
+++ 01_the_basics/exercise_03.rb
@@ -0,0 +1,5 @@
+movies = {:The_Man_Of_Steel => 2013, :The_Dark_Knight => 2008, :The_Dark_Knight_Rises => 2012}
+
+puts movies[:The_Man_Of_Steel]
+puts movies[:The_Dark_Knight]
+puts movies[:The_Dark_Knight_Rises]
