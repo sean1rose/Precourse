3e463330878ec4d098f7db1f421cb8ce58c72b71
Sean Rose <sean1rose@gmail.com>
2014-05-09 19:29:27 -0700

    Explain what the error message means

---
+++ 01_the_basics/exercise_07.rb
@@ -0,0 +1,5 @@
+# What does the following error message tell you?
+# SyntaxError: (irb):2: syntax error, unexpected ')', expecting '}'
+# from /usr/local/rvm/rubies/ruby-2.0.0-rc2/bin/irb:16:in `<main>'
+
+puts "It tells me that on line 2 of the code, a closing hash was expected as opposed to a closing parenthesis/array"
