1f9a5502f1c292372f8511e4ec8be92326ad1eb7
Sean Rose <sean1rose@gmail.com>
2014-05-10 14:17:49 -0700

    What's the error msg for?

---
+++ 03_methods/exercise_06.rb
@@ -0,0 +1,10 @@
+# What does the following error msg tell you?...
+
+# ArgumentError: wrong number of arguments (1 for 2)
+# from (irb):1:in `calculate_product'
+# from (irb):4
+# from /Users/username/.rvm/rubies/ruby-2.0.0-p353/bin/irb:12:in `<main>'
+
+puts "It's telling me that I called a method using an incorrect number of arguments."
+puts "This happened in the method 'calculate_product' on line 1 of the code."
+puts "The incorrect arg is used on line 4"
