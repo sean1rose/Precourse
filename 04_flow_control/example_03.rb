67cc8d1afee300c19eb00c70cf084b004a089da2
Sean Rose <sean1rose@gmail.com>
2014-05-12 20:47:37 -0700

    Addded #{}

--- 04_flow_control/example_03.rb
+++ 04_flow_control/example_03.rb
@@ -3,9 +3,9 @@ puts "Pick a # b/w 0 and 100 foo!"
 user_answer = gets.chomp.to_i
 
 if (user_answer >= 0 && user_answer < 50)
-  puts "That's b/w 0 and 50 foo"
+  puts "#{user_answer} is b/w 0 and 50 foo"
 elsif (user_answer >= 50 && user_answer < 100)
-  puts "That's b/w 50 and 100 foo"
+  puts "#{user_answer} is b/w 50 and 100 foo"
 elsif (user_answer >= 100)
-  puts "That's above 100 foo"
+  puts "#{user_answer} is above 100 foo"
 end
