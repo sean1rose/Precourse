b41f9df6ef95d031480c327db76a3b508138b0d4
Sean Rose <sean1rose@gmail.com>
2014-05-15 14:40:07 -0700

    array index/value error

---
+++ 06_arrays/exercise_06.rb
@@ -0,0 +1,7 @@
+names = ['bob', 'joe', 'susan', 'margaret']
+# names['margaret'] = 'jody'
+
+# Assuming that you're trying to replace the name 'margaret' with jody'...
+# ...The problem is that you need to use... 
+names[3] == 'jody'
+# aka you need to use the index number and not the value, in order to replace it
