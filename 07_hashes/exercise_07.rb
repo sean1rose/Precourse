951361f0a4f3232838ecdc33da88e7ba672222ec
Sean Rose <sean1rose@gmail.com>
2014-05-15 23:14:14 -0700

    difference between two hashes

---
+++ 07_hashes/exercise_07.rb
@@ -0,0 +1,8 @@
+x = "hi there"
+
+my_hash = {x: "some value"}
+my_hash2 = {x => "some value"}
+
+# The difference b/w the 2...
+# my_hash uses a symbol, x, as the key
+# my_hash2 uses the string 'hi there' as the key
